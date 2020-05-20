318fbd93e7183b7e4007224ffb347dcb53bb5d94
Joana H <joana_hrotko@hotmail.com>
2020-05-20 23:13:43 +0100

    Refactor
    remove div_level_table
    
    clean ktreeExtended

--- include/ktree_extended.hpp
+++ include/ktree_extended.hpp
@@ -23,40 +23,16 @@ public:
     ktree_extended() : k_tree() {}
 
     ktree_extended(size_t n_vertices) : k_tree() {
-        initialize(n_vertices);
+        this->n_vertices = n_vertices;
     }
 
     ktree_extended(vector<tuple<k2_tree_ns::idx_type, k2_tree_ns::idx_type>> edges,
                    const size_type size) : k_tree(edges, size) {
-        uint max_v = 0;
-        //FIXME: LOSES PERFORMANCE
-        for (size_t i = 0; i < edges.size(); i++) {
-            max_v = max(max_v, get<0>(edges[i]));
-            max_v = max(max_v, get<1>(edges[i]));
-        }
-        if (max_v > size)
-            throw logic_error("Incorrect size. It must be higher than the highest node");
-
-        initialize(size);
+        this->n_vertices = size;
     }
 
     ktree_extended(k_tree tree, const size_t n_vertices) : k_tree(tree) {
-        initialize(n_vertices);
-    }
-
-
-    void edge_iterator(function<int(uint, uint)> callback) {
-        vector<ulong> pointerL;
-        pointerL.resize(max_level);
-        pointerL.push_back(0);
-        pointerL.push_back(k * k);
-
-        for (size_t i = 2; i <= max_level; i++) {
-            ulong a = (this->k_l_rank(pointerL[i - 1] - 1) + 1) * k * k;
-            pointerL.push_back(a);
-        }
-
-        recursive_edge_iterator(pointerL, 0, 0, -1, -1, callback);
+        this->n_vertices = n_vertices;
     }
 
     ktree_extended unionOp(k_tree &k_tree, uint n_vertices) {
@@ -64,48 +40,7 @@ public:
         return ktree_extended(res, n_vertices);
     }
 
-    void set_level(vector<int> div_level_table) {
-        this->div_level_table = div_level_table;
-    }
-
 protected:
-    void initialize(uint size) {
-        n_vertices = size;
-        uint aux = log(n_vertices) / log(k);
-        max_level = floor(aux) == aux ? floor(aux) - 1 : floor(aux);
-
-        for (size_t i = 0; i <= max_level; i++)
-            div_level_table.push_back(pow(k, max_level - i));
-    }
-
-    void
-    recursive_edge_iterator(vector<ulong> pointerL, uint dp, uint dq, uint x, int l, function<int(uint, uint)> proc) {
-        uint y;
-        if (l == max_level)
-            if (this->k_l[x])
-                proc(dp, dq);
-
-        if ((l == max_level - 1) && (this->k_l[x])) {
-            y = pointerL[l + 1];
-            pointerL[l + 1] += k * k;
-            for (size_t i = 0; i < k; i++)
-                for (size_t j = 0; j < k; j++)
-                    recursive_edge_iterator(pointerL, dp + i, dq + j, y + k * i + j, l + 1, proc);
-        }
-        if ((x == -1) || ((l < max_level - 1) && (this->k_l[x]))) {
-            y = pointerL[l + 1];
-            pointerL[l + 1] += k * k;
-
-            uint div_level = div_level_table[l + 1];
-            for (size_t i = 0; i < k; i++)
-                for (size_t j = 0; j < k; j++)
-                    recursive_edge_iterator(pointerL, dp + div_level * i, dq + div_level * j, y + k * i + j, l + 1,
-                                            proc);
-        }
-    }
-
-    vector<int> div_level_table;
-    uint max_level;
     uint n_vertices;
 };
 
