ef65e79d1342c754ea133dbe6f8f83a5dce70885
joo95h <joana_hrotko@hotmail.com>
2020-08-12 18:23:17 +0100

    Fix serialize creation files of the ktrees

--- test/heavy_test.cpp
+++ test/heavy_test.cpp
@@ -14,8 +14,10 @@ void split(const std::string &str, vector<string> &cont,
 }
 
 TEST(ReadTest, ReadFromDataset) {
+    std::stringstream ss;
     unsigned int n_vertices = 50000;
     std::ostringstream path;
+
     path << "datasets/" << n_vertices << "/" << n_vertices << ".tsv";
     ifstream test_case (path.str());
 
@@ -33,10 +35,37 @@ TEST(ReadTest, ReadFromDataset) {
             etype y = (etype) stoi(substrings[2]);
             if (substrings[0] == "a") {
                 graph.add_edge(x, y);
-//                cout << "number of edges: " << graph.get_number_edges() << endl;
             }
         }
+        graph.serialize(ss);
         test_case.close();
+    } else  {
+        cout << "Unable to open file";
+        FAIL();
+    }
+
+    std::ostringstream path_delete;
+    path_delete << "datasets/" << n_vertices << "/" << n_vertices << "-deletions.tsv";
+    ifstream test_case_delete (path_delete.str());
+
+    if (test_case_delete.is_open()) {
+        std::string line;
+        vector<std::string> substrings;
+        const std::string delims = " ";
+
+        dynamic_ktree::DKtree<2> graph;
+        graph.load(ss);
+        while (getline (test_case_delete, line))
+        {
+            split(line, substrings, delims);
+
+            etype x = (etype) stoi(substrings[1]);
+            etype y = (etype) stoi(substrings[2]);
+            if(substrings[0] == "d") {
+                graph.del_edge(x,y);
+            }
+        }
+        test_case_delete.close();
     } else  {
         cout << "Unable to open file";
         FAIL();
